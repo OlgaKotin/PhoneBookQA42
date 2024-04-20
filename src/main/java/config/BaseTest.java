b6a9aec78f8db7953ea51902a5788a5e89e1c971
Olga Kotin <o.b.baglay@gmail.com>
2024-04-20 20:39:34 +0300

    Test loginOfAnExistingUserAddContact

--- src/main/java/config/BaseTest.java
+++ src/main/java/config/BaseTest.java
@@ -1,5 +1,62 @@
 package config;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
+
 public class BaseTest {
+
+    private WebDriver driver;
+
+    public WebDriver getDriver(){
+        return driver;
+    }
+
+    @BeforeMethod
+    @Parameters("browser")
+    public void setUp(@Optional("firefox") String browser){
+        if(browser.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--lang=en");
+            driver = new ChromeDriver(options);
+        }else if(browser.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            FirefoxOptions options =new FirefoxOptions();
+            options.addPreference("intl.accept_languages","en");
+            driver = new FirefoxDriver(options);
+        }else if(browser.equalsIgnoreCase("edge")){
+            WebDriverManager.edgedriver().setup();
+            EdgeOptions options = new EdgeOptions();
+            driver = new EdgeDriver();
+        }
+        else {throw new IllegalArgumentException("Invalid browser value : "+browser);}
+        driver = getDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+    }
+    @AfterMethod
+    public void tearDown(){
+        WebDriver driver1 = getDriver();
+        if(driver1 != null){
+            driver1.quit();
+        }
+    }
 
 }
